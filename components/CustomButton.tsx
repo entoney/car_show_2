af7801c74ff3cdb3d735e3d97aab946d4e13fade
entoney <e_ntoney@yahoo.com>
2023-07-07 18:20:17 -0500

    Added the hero, footer, styled buttons

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { CustomButtonProps } from '@/types';
 import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
+
 
 const CustomButton = ({ title, containerStyles, 
-handleClick }: CustomButtonProps) => {
+handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
-        type={"button"}
+        type={btnType || "button"}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
 
       <span className={`felz-1`}>
-        title
+        {title}
       </span>
     </button>
   )
