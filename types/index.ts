0ef0970fc97ba1064478717c0169ac02d4506bce
entoney <e_ntoney@yahoo.com>
2023-07-10 20:05:10 -0500

    Added CustomFilter to fuel and year.

--- types/index.ts
+++ types/index.ts
@@ -32,10 +32,21 @@ year: number;
 
 }
 
-export interface FilterProps{
+export interface FilterProps {
     manufacturer: string;
     year: number;
     fuel: string;
     limit: number;
     model: string;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+
 }
