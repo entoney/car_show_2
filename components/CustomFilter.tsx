6e09491f2888dae90b6a26e38fd17673eb2661dc
entoney <e_ntoney@yahoo.com>
2023-07-11 19:42:20 -0500

    updated fuel bar

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,15 +5,25 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
 const [selected, setSelected] = useState(options[0]);
+
+const handleUpdateParams = (e: { title: string, value: string }) => {
+  const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+  router.push(newPathName);
+}
 
   return (
     <div className="w-fit">
       <Listbox
       value={selected}
-      onChange={(e) => setSelected(e)}>
+      onChange={(e) => {setSelected(e);
+      handleUpdateParams(e);
+    }}>
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">            
               <span className="block truncate">{selected.title}</span>
@@ -42,7 +52,7 @@ const [selected, setSelected] = useState(options[0]);
               }`}
               >
               {({ selected }) => (
-                <span>{option.title}</span>
+                <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{option.title}</span>
               )}
             </Listbox.Option>
             ))}
